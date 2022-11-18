45e21e4df637c9ace58cce26196a8a48e20728a8
Nicolás Hdez Glez <alu0100898293@ull.edu.es>
2022-11-18 12:55:32 +0000

    App title updated

--- app_visualizacion.py
+++ app_visualizacion.py
@@ -5,6 +5,8 @@ from src.maquetado import vistas
 # change max message size which can be sent via websocket
 # st.server.server_util.MESSAGE_SIZE_LIMIT = 300 * 1e6
 
+st.set_page_config(page_title="Datastrofe")
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # navigation links
